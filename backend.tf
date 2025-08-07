5ca7eec1ee45e41ecbf7e1198cb7ae6b2b85855c
mattenarle10 <enarlem10@gmail.com>
2025-08-07 23:11:53 +0800

    chore: update

--- backend.tf
+++ backend.tf
@@ -1,14 +1,9 @@
-# This file will be configured after creating the S3 bucket
-# Uncomment and update the values after creating your S3 bucket
-
-/*
 terraform {
   backend "s3" {
-    bucket         = "matt-terraform-bucket"
+    bucket         = "matt-terraform-bucket-ecv"
     key            = "state/terraform.tfstate"
     region         = "us-east-2"
     encrypt        = true
     # dynamodb_table = "matt-tf-locks"  # Uncomment if you decide to use DynamoDB for state locking
   }
 }
-*/
